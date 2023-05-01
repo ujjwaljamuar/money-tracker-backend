5c5f88f4d7034301a3238a86e16ce8b8b8deb842
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-05-01 11:28:43 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+// import modules
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+// app
+const app = express();
+const Transaction = require("./modals/Transaction.js");
+const mongoose = require("mongoose");
+
+// midldleware
+app.use(cors());
+app.use(express.json());
+app.get("/api/test", (req, res) => {
+  res.json("test ok");
+});
+
+// db
+app.post("/api/transaction", async (req, res) => {
+  await mongoose.connect(process.env.MONGO_URL);
+  const { name, price, description, datetime } = req.body;
+
+  const transaction = await Transaction.create({
+    name,
+    price,
+    description,
+    datetime,
+  });
+  
+  res.json(transaction);
+});
+
+app.get('/api/transactions', async (req, res) => {
+  await mongoose.connect(process.env.MONGO_URL);
+  const transactions = await Transaction.find();
+
+  res.json(transactions);
+})
+
+
+// port || listener
+app.listen(process.env.PORT);
