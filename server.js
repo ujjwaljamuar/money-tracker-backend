c8b012415f80c3bb9cf741b5a2744f84db2d78b4
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-05-01 14:52:41 +0530

    sorted json in descending order

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post("/api/transaction", async (req, res) => {
 
 app.get('/api/transactions', async (req, res) => {
   await mongoose.connect(process.env.MONGO_URL);
-  const transactions = await Transaction.find().sort();
+  const transactions = await Transaction.find().sort({ datetime: -1 });
 
   res.json(transactions);
 })
