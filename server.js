54593d35aa14eb33aa43485945b6e83dc4f732bf
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-05-06 19:29:44 +0530

    *

--- server.js
+++ server.js
@@ -12,31 +12,30 @@ const mongoose = require("mongoose");
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.json("Backend Server is running ...");
+    res.json("Backend Server is running ...");
 });
 
 // db
 app.post("/api/transaction", async (req, res) => {
-  await mongoose.connect(process.env.MONGO_URL);
-  const { name, price, description, datetime } = req.body;
+    await mongoose.connect(process.env.MONGO_URL);
+    const { name, price, description, datetime } = req.body;
 
-  const transaction = await Transaction.create({
-    name,
-    price,
-    description,
-    datetime,
-  });
-  
-  res.json(transaction);
+    const transaction = await Transaction.create({
+        name,
+        price,
+        description,
+        datetime,
+    });
+
+    res.json(transaction);
 });
 
-app.get('/api/transactions', async (req, res) => {
-  await mongoose.connect(process.env.MONGO_URL);
-  const transactions = await Transaction.find().sort({ datetime: -1 });
+app.get("/api/transactions", async (req, res) => {
+    await mongoose.connect(process.env.MONGO_URL);
+    const transactions = await Transaction.find().sort({ datetime: -1 });
 
-  res.json(transactions);
-})
+    res.json(transactions);
+});
 
-
-// port || listener 
+// port || listener
 app.listen(process.env.PORT || 4040);
